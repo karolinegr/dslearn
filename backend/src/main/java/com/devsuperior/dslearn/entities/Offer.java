c323e0b0caa02071b67b302fbf33512294ab170c
Karoline <karolgribeiro@gmail.com>
2023-08-22 23:04:27 -0300

    - create course, offer, resource and section entities; - db seeding;

---
+++ backend/src/main/java/com/devsuperior/dslearn/entities/Offer.java
@@ -0,0 +1,100 @@
+package com.devsuperior.dslearn.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_offer")
+public class Offer implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String edition;
+    private Instant startMoment;
+    private Instant endMoment;
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @OneToMany(mappedBy = "offer")
+    private List<Resource> resources = new ArrayList<>();
+
+    public Offer() {
+    }
+
+    public Offer(Long id, String edition, Instant startMoment, Instant endMoment, Course course) {
+        this.id = id;
+        this.edition = edition;
+        this.startMoment = startMoment;
+        this.endMoment = endMoment;
+        this.course = course;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public Instant getStartMoment() {
+        return startMoment;
+    }
+
+    public void setStartMoment(Instant startMoment) {
+        this.startMoment = startMoment;
+    }
+
+    public Instant getEndMoment() {
+        return endMoment;
+    }
+
+    public void setEndMoment(Instant endMoment) {
+        this.endMoment = endMoment;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public List<Resource> getResources() {
+        return resources;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Offer offer = (Offer) o;
+        return Objects.equals(id, offer.id) && Objects.equals(edition, offer.edition) && Objects.equals(startMoment, offer.startMoment) && Objects.equals(endMoment, offer.endMoment) && Objects.equals(course, offer.course);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, edition, startMoment, endMoment, course);
+    }
+}
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -12,3 +12,19 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp SpringBoot', 'https://pixabay.com/photos/laptop-woman-education-study-young-3087585/', 'https://pixabay.com/photos/laptop-woman-education-study-young-3087585/');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', TIMESTAMP WITH TIME ZONE '2021-07-14T10:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-14T10:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-14T10:00:00Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha', 'Trilha de aulas', 1, 'https://pixabay.com/photos/laptop-woman-education-study-young-3087585/', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'https://pixabay.com/photos/laptop-woman-education-study-young-3087585/', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://pixabay.com/photos/laptop-woman-education-study-young-3087585/', 0, 1);
+
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Aula 1', 'Aula', 1, 'https://pixabay.com/photos/laptop-woman-education-study-young-3087585/', 1, null);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Aula 2', 'Aula', 2, 'https://pixabay.com/photos/laptop-woman-education-study-young-3087585/', 1, 1);
+INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Aula 3', 'Aula', 3, 'https://pixabay.com/photos/laptop-woman-education-study-young-3087585/', 1, 2);
+
+
+-- INSERT INTO () VALUES ();
