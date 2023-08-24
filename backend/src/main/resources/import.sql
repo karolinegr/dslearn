1e27e454fe28466185b964cab087a844b6d5bf2b
Karoline <karolgribeiro@gmail.com>
2023-08-23 23:12:57 -0300

    - database seeding;

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -26,5 +26,7 @@ INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prer
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Aula 2', 'Aula', 2, 'https://pixabay.com/photos/laptop-woman-education-study-young-3087585/', 1, 1);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Aula 3', 'Aula', 3, 'https://pixabay.com/photos/laptop-woman-education-study-young-3087585/', 1, 2);
 
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2020-07-14T13:00:00Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2020-07-14T13:00:00Z', null, true, false);
 
 -- INSERT INTO () VALUES ();
