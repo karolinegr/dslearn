7c241ee16aa32f0acd39126b5875258ed456f838
Karoline <karolgribeiro@gmail.com>
2023-08-29 23:27:56 -0300

    - create repositories;

---
+++ backend/src/main/java/com/devsuperior/dslearn/repositories/NotificationRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslearn.repositories;
+
+import com.devsuperior.dslearn.entities.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
---
+++ backend/src/main/java/com/devsuperior/dslearn/repositories/SectionRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslearn.repositories;
+
+import com.devsuperior.dslearn.entities.Section;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SectionRepository extends JpaRepository<Section, Long> {
+}
